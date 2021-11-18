f70c7bd22c859fddfd1b32b7def1ff26ebf3a144
Marcela Rojas <marcelarojas_d@outlook.com>
2021-11-17 21:03:12 -0500

    avance en interfaz de login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import PrivateLayout from 'layouts/PrivateLayout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { UserContext } from 'context/userContext';
+import Login from 'pages/login';
 import Index from 'pages/Index';
 import Page2 from 'pages/Page2';
 import IndexCategory1 from 'pages/category1/Index';
@@ -24,12 +25,7 @@ function App() {
       <UserContext.Provider value={{ userData, setUserData }}>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<PrivateLayout />}>
-              <Route path='' element={<Index />} />
-              <Route path='page2' element={<Page2 />} />
-              <Route path='category1' element={<IndexCategory1 />} />
-              <Route path='category1/page1' element={<Category1 />} />
-            </Route>
+            <Route path='Login' element={<Login/>}></Route>
           </Routes>
         </BrowserRouter>
       </UserContext.Provider>
