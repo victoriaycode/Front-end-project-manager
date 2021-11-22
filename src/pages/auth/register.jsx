07b6ebba04c7667b4606721f284d66bd02824e58
Marcela Rojas <marcelarojas_d@outlook.com>
2021-11-22 18:12:56 -0500

    se ajuste ubicacion de form, pdte ubicacion boton volver a login

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -41,38 +41,38 @@ const Register = () => {
     return (
     <div>       
         <div className='bg-blue-800 flex flex-col h-screen text-white'>
-            <div className='ml-96'> 
+            <div className='ml-96 pl-80'> 
                 <Link to ='/auth/login'>
-                    <button className='bg-yellow-500 hover:bg-yellow-600 p-2 w-96 border-none text-white rounded-lg h-auto my-8 cursor-pointer text-2xl'>                
+                    <button className='bg-yellow-500 hover:bg-yellow-600 p-2 w-auto border-none text-white rounded-lg h-auto my-8 cursor-pointer text-2xl'>                
                     Volver a Login
                     </button>
                 </Link>
                     <h1 className='text-3xl font-bold '>Registrar nuevo usuario</h1>
-                    <p className='text-2xl mt-2'>Ingresa los datos del nuevo usuario→</p>
-                    <form className='flex flex-col mt-16'> 
+                    <p className='text-2xl'>Ingresa los datos del nuevo usuario →</p>
+                    <form className='flex flex-col mt-12'> 
                         
                         <label className='font-bold text-xl' htmlFor="NombreUsuario">Nombres</label>
                         <input type="text" name="nombre" 
-                        className='mt-2 rounded-lg p-2 h-12 w-96 cursor-auto outline-none text-blue-900 text-xl'
+                        className='mt-2 rounded-lg p-2 max-w-lg cursor-auto outline-none text-blue-900 text-xl'
                         placeholder="Ingresa nombre completo" required/><br/>
 
                         <label className='font-bold text-xl' htmlFor="ApellidorUsuario">Apellidos </label>
                         <input type="text" name="apellido"
-                        className='mt-2 rounded-lg p-2 h-12 w-96 cursor-auto outline-none text-blue-900 text-xl'
+                        className='mt-2 rounded-lg p-2 max-w-lg cursor-auto outline-none text-blue-900 text-xl'
                         required/><br/>
                     
                         <label className='font-bold text-xl' htmlFor="id">Cédula</label>
                         <input type="numer" name="identificacion"
-                        className='mt-2 rounded-lg p-2 h-12 w-96 cursor-auto outline-none text-blue-900 text-xl'
+                        className='mt-2 rounded-lg p-2 max-w-lg cursor-auto outline-none text-blue-900 text-xl'
                         required/><br/>
 
                         <label className='font-bold text-xl' htmlFor="emailUsuario">Correo</label>
                         <input type="email" id="email" name="correo"
-                        className='mt-2 rounded-lg p-2 h-12 w-96 cursor-auto outline-none text-blue-900 text-xl'
+                        className='mt-2 rounded-lg p-2 max-w-lg cursor-auto outline-none text-blue-900 text-xl'
                         placeholder="Ejemplo: hola@hotmail.com" required/><br/>
                         
                         <label className='font-bold text-xl' htmlFor="rolUsuario">Rol del Usuario</label>
-                            <select className='mt-2 rounded-lg p-2 h-12 w-96 cursor-auto outline-none text-blue-900 text-xl'
+                            <select className='mt-2 rounded-lg p-2 max-w-lg cursor-auto outline-none text-blue-900 text-xl'
                             name="rol" required defaultValue={0} >
                                 <option disabled value={0}> Selecciona un rol</option>
                                 <option>Administrador</option>
@@ -82,7 +82,7 @@ const Register = () => {
 
                         <input id="input_ventas" type="hidden" name="estado" value="Pendiente" required/>
                         
-                        <button type="submit" className='bg-yellow-500 hover:bg-yellow-600 p-2 w-96 border-none text-white rounded-lg h-auto my-8 cursor-pointer text-2xl'
+                        <button type="submit" className='bg-yellow-500 hover:bg-yellow-600 p-2 max-w-lg border-none text-white rounded-lg h-auto my-8 cursor-pointer text-2xl'
                         > Registrarme! </button>
                         </form>
                     {/* <ToastContainer position='bottom-center' autoClose={4000} />     */}
