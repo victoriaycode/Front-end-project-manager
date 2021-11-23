060b91181abb37df738f6434d10fe06cbd75a347
Yeison Buitrago <yeison_buitragos@hotmail.com>
2021-11-23 17:59:20 -0500

    se modifico la tabla de usuarios y se realizo el query a la base de datos apra traer los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PrivateLayout from 'layouts/PrivateLayout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
 import { UserContext } from 'context/userContext';
 import Index from 'pages/Index';
 
@@ -17,6 +16,12 @@ import Students from 'pages/projects/Students';
 import ProjectsList from 'components/ProjectsList';
 import Advance from 'pages/projects/advances/Advance';
 import New_project from 'pages/projects/New_project';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://backend-dev-ocean.herokuapp.com/graphql', 
+  cache: new InMemoryCache(),
+});
 
 
 // import PrivateRoute from 'components/PrivateRoute';
@@ -25,33 +30,28 @@ function App() {
   const [userData, setUserData] = useState({});
 
   return (
-    <Auth0Provider
-      domain='misiontic-concesionario.us.auth0.com'
-      clientId='WsdhjjQzDLIZEHA6ouuxXGxFONFGAQ4g'
-      redirectUri='http://localhost:3000/admin'
-      audience='api-autenticacion-concesionario-mintic'
-    >
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<PrivateLayout />}>
-              <Route path='/' element={<Index />} />
-              <Route path='/usuarios' element={<UsersDashboard />} />
-              <Route path='/proyectos' element={<ProjectsList />} />
-              <Route path='/proyectos/misproyectos' element={<ProjectDashboard />} />
-              <Route path='/proyectos/proyecto' element={<Project />} />
-              <Route path='/proyectos/nuevo' element={<New_project />} />
-              <Route path='/proyectos/proyecto/avances' element={<AdvancesDashboard />} />
-              <Route path='/proyectos/proyecto/avances/avance' element={<Advance />} />
-              <Route path='/proyectos/proyecto/info' element={<Project />} />
-              <Route path='/proyectos/proyecto/estudiantes' element={<Students />} />
-              
-              <Route path='/mi_perfil' element={< Profile/>} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </Auth0Provider>
+    <ApolloProvider client={client}>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<PrivateLayout />}>
+                <Route path='/' element={<Index />} />
+                <Route path='/usuarios' element={<UsersDashboard />} />
+                <Route path='/proyectos' element={<ProjectsList />} />
+                <Route path='/proyectos/misproyectos' element={<ProjectDashboard />} />
+                <Route path='/proyectos/proyecto' element={<Project />} />
+                <Route path='/proyectos/nuevo' element={<New_project />} />
+                <Route path='/proyectos/proyecto/avances' element={<AdvancesDashboard />} />
+                <Route path='/proyectos/proyecto/avances/avance' element={<Advance />} />
+                <Route path='/proyectos/proyecto/info' element={<Project />} />
+                <Route path='/proyectos/proyecto/estudiantes' element={<Students />} />
+                
+                <Route path='/mi_perfil' element={< Profile/>} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </UserContext.Provider>
+    </ApolloProvider>
   );
 }
 
