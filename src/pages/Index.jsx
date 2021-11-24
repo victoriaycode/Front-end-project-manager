5a6d11f5953c8817ce535098adccc825576b2216
Victoria Yuan <90482772+victoriaycode@users.noreply.github.com>
2021-11-23 19:08:39 -0500

    Revert "HU_004_BD"

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,13 @@
-import Title_page from 'components/Title_page';
 import React from 'react';
 
 const Index = () => {
   return (
-    <div className="w-full h-full flex flex-col overflow-y-hidden " >
-       <Title_page title={"Inicio"}  returns ={false} return_to={"/"}></Title_page>
-
-        </div>
+    <div>
+      <div className='bg-green-300 h-96'>Index page</div>
+      <div className='bg-green-300 h-96'>Index page</div>
+      <div className='bg-green-300 h-96'>Index page</div>
+      <div className='bg-green-300 h-96'>Index page</div>
+    </div>
   );
 };
 
