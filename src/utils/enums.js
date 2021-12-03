c4deb7c39b5dd6daf906ae4b3a586f14b4e7da35
Felipe Sierra <felipesierrae@gmail.com>
2021-12-02 20:25:13 -0500

    Code profe

---
+++ src/utils/enums.js
@@ -0,0 +1,18 @@
+const Enum_Rol = {
+  ADMINISTRADOR: 'Administrador',
+  ESTUDIANTE: 'Estudiante',
+  LIDER: 'Líder',
+};
+
+const Enum_EstadoUsuario = {
+  PENDIENTE: 'Pendiente',
+  AUTORIZADO: 'Autorizado',
+  NO_AUTORIZADO: 'No autorizado',
+};
+
+const Enum_EstadoProyecto = {
+  ACTIVO: 'Activo',
+  INACTIVO: 'Inactivo',
+};
+
+export { Enum_Rol, Enum_EstadoUsuario, Enum_EstadoProyecto };
