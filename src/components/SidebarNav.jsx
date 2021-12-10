d309d5e18aa6785f48b3d6728c6f633c980c4423
victoriaycode <yc.victoria1@gmail.com>
2021-12-10 04:01:46 -0500

    a little change in style of sidebar

--- src/components/SidebarNav.jsx
+++ src/components/SidebarNav.jsx
@@ -9,7 +9,7 @@ import { Tooltip } from '@material-ui/core';
 const SidebarLinks = () => {
 
     return (
-      <ul className="flex flex-col space-y-2 w-full items-center pt-5 mt-10">
+      <ul className="flex flex-col space-y-2 w-full items-center  pt-5 mt-10">
         
       <SidebarRoute1 to='/inicio' title='Inicio' icon="fas fa-home fa-lg"/>
       <SidebarRoute1 to='/proyectos' title='Proyectos' icon="fas fa-folder fa-lg"/>
@@ -17,14 +17,14 @@ const SidebarLinks = () => {
       <SidebarRoute1 to='/inscripciones' title='Inscripciones' icon="fas fa-clipboard-check fa-lg"/>
      </PrivateComponent>
       <SidebarRoute1 to='/usuarios' title='Usuarios' icon="fas fa-users fa-lg"/>
-      <SidebarRoute1 to='/mi_perfil' title='Mi Perfil' icon="fas fa-user-cog fa-lg"/>
+      <SidebarRoute1 to='/mi_perfil' title='Mi_Perfil' icon="fas fa-user-cog fa-lg"/>
       </ul>
     );
   };
   
   const Logo = () => {
     return (
-      <div className='py-3 w-full flex flex-col items-center justify-center'>
+      <div className='py-3 w-full flex flex-col items-center justify-center  '>
         <img src='logo1.png' alt='Logo' className='h-16' />
         <span className='my-2 text-xl font-bold text-center'>Gestión de Proyectos</span>
       </div>
@@ -41,14 +41,14 @@ const SidebarNav = () => {
 
     return (
 
-      <div className="flex flex-col justify-between  items-center flex-none w-20 bg-gray-100 mr-4">
-        <div className="rounded-2xl ml-5 mt-3 mb-3   w-full h-full bg-white  ">
+      <div className="flex flex-col justify-between  items-center flex-none w-24 bg-gray-100 mr-2">
+        <div className="rounded-2xl ml-5 my-3   w-full h-full bg-white  ">
      
       <div className="flex flex-col space-y-2 w-full items-center pt-5 mt-10 " >
       <SidebarLinks/>
         
       </div>
-      <div className="flex flex-col space-y-4 pb-5 pr-2 mt-24">
+      <div className="flex flex-col space-y-4 pb-5 pr-2 mt-20">
       
         <a className="block relative flex flex-col w-full h-16 w-16 flex justify-center items-center text-blue-800 
           hover:text-blue-400">
@@ -86,13 +86,13 @@ const SidebarRoute1 = ({ to, title, icon }) => {
         to={to}
         className={({ isActive }) =>
           isActive
-            ? 'sidebar-route text-blue-500 bg-gray-100 rounded-2xl '
-            : 'sidebar-route text-gray-400'
+            ? 'sidebar-route text-blue-500  font-bold   border-b-4 border-blue-400'
+            : 'sidebar-route text-gray-400  border-b-2 border-gray-100'
         }>
-          <div className=" relative flex flex-col w-full h-16 flex justify-center items-center   
-         hover:text-blue-400  cursor-pointer transform transition duration-100 ">
+          <div className=" relative flex flex-col text-center w-full h-16 flex justify-center items-center   
+         hover:text-blue-400  cursor-pointer transform transition duration-50 p-2">
         <i className={icon} />
-        <span className="text-xs mt-1">{title}</span> 
+        <span className="text-sm mt-1">{title}</span> 
         </div>
           
       </NavLink>
