cef573fa098ad49e4562e4eaa36bd355b7f31e40
victoriaycode <yc.victoria1@gmail.com>
2021-12-11 00:21:43 -0500

    Table for advances

--- src/pages/projects/advances/AdvancesDashboard.jsx
+++ src/pages/projects/advances/AdvancesDashboard.jsx
@@ -1,6 +1,6 @@
 
 import ProjectNavbar from 'components/ProjectNavbar'
-import AdvancesTable from 'components/AdvancesTable'
+
 import React from 'react'
 import New_advance_modal from 'components/New_advance_modal'
 import { useState, useEffect } from 'react'
@@ -8,12 +8,13 @@ import { useParams } from 'react-router-dom'
 import { useUser } from 'context/userContext'
 import { useQuery } from '@apollo/client'
 import { GET_INSCRIPCION_ACEPTADA } from 'graphql/inscripciones/queries'
+import TableAdvances from './TableAdvances'
 
 const AdvancesDashboard = () => {
   const { userData } = useUser();
     const { _id } = useParams();
     const name_student= userData.nombre +" "+ userData.apellido;
-    const idEstudiante= userData._id +" ";  
+    const idEstudiante= userData._id +"";  
     const idProyecto= _id;
     const { data,  error, loading } = useQuery(GET_INSCRIPCION_ACEPTADA, {
       variables: {
@@ -31,12 +32,13 @@ const AdvancesDashboard = () => {
     const [openNewAdvanceModal, setNewAdvanceModal]= useState(false);
 
     return (
-        <div className="w-full h-full flex flex-col overflow-y-hidden " >
+        <div className="w-full h-full flex flex-col overflow-y-hidden  bg-gray-100" >
       <ProjectNavbar _idActual={_id} nombreProject={_id} rutaRetorno={`/proyectos/proyecto/${_id}`}/>
    
       
    {/*<AdvancesTable idProject={_id} openNewAdvanceModal={openNewAdvanceModal} setModal={setNewAdvanceModal}></AdvancesTable>*/ } 
-    <AdvancesTable idProject={_id} openNewAdvanceModal={openNewAdvanceModal} setModal={setNewAdvanceModal}></AdvancesTable>
+    {/* <AdvancesTable idProject={_id} openNewAdvanceModal={openNewAdvanceModal} setModal={setNewAdvanceModal}></AdvancesTable> */}
+    <TableAdvances idProject={_id}openNewAdvanceModal={openNewAdvanceModal} setModal={setNewAdvanceModal} ></TableAdvances>
     {openNewAdvanceModal &&
      <New_advance_modal nameStudent={name_student}  idStudent={idEstudiante} 
      idProject={_id} setOpenModal={setNewAdvanceModal}></New_advance_modal>} 
